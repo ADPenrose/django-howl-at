5433eede852e94e3e1a20b69539f3a1fb1ad73b9
Arturo Avila <mysticmetromx@gmail.com>
2024-02-18 00:38:29 -0600

    Added TailwindCSS

---
+++ build.sh
@@ -0,0 +1,4 @@
+npm install
+npm run tailwind-build
+pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/templates/**/*.html', './src/**/static/**/*.js'],
+	theme: {
+		extend: {},
+	},
+};
