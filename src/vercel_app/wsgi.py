4850c4d829032bb16d7ceb1cb21a3718c18f4738
Arturo Avila <mysticmetromx@gmail.com>
2024-02-18 11:11:53 -0600

    Added env vars

--- src/vercel_app/wsgi.py
+++ src/vercel_app/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vercel_app.settings")
+if os.environ.get("ENV") == "production":
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 app = get_wsgi_application()
