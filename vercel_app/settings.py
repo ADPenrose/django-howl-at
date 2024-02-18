4a7c0db643d89b4cd3f47992f69e0962db9817b1
Arturo Avila <mysticmetromx@gmail.com>
2024-02-18 00:23:05 -0600

    Changed app export name

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -68,18 +68,13 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "vercel_app.wsgi.application"
+WSGI_APPLICATION = "vercel_app.wsgi.app"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {}
 
 
 # Password validation
